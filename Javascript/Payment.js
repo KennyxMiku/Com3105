cf0068b0497e0c9e68727b80884e8ae7d63f6a8f
KennyxMiku <s215408@hsu.edu.hk>
2022-11-16 13:03:42 +0800

    done

--- Javascript/Payment.js
+++ Javascript/Payment.js
@@ -1,23 +1,5 @@
-function process() {
-		var price = [299.00, 299.00, 299.00, 299.00];
-		var qty = 0;
-		var itemtot = 0;
-		var totsold = 0;
-		var i;
-		for (i = 0; i <= 3; i++)  {
-			qty = parseInt(document.querySelector("#qty" + i).value);
-			if (isNaN(qty)) {
-				;
-			} else {
-				itemtot = qty * price[i];
-				document.querySelector("#itemtot" + i).value = itemtot.toFixed(2);
-				totsold += itemtot;
-			}
-		}
-		document.querySelector("#totsold").value = totsold.toFixed(2);
-	}
-	
-function Paypal() {	
+
+function paypal() {	
 		alert("Execute Paypal()...");
-		var aprvlWin = window.open("com3105_approval.html", "approvalWin", width="180", height="150");
+		var aprvlWin = window.open("http://localhost:3000/check_out?username=", "approvalWin", width="180", height="150");
 }
